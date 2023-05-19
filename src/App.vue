f6987401b54fd56402794cabf994f852dfa74a65
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-19 13:14:51 -0500

    base del proyecto creada y estilada

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="header">
+    <nav class="navbar__container">
+      <RouterLink class="navbar__link" to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import url(app.css);
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SalaryCalculator from '../components/SalaryCalculator.vue';
+</script>
+
+<template>
+  <main>
+    <SalaryCalculator />
+  </main>
+</template>
