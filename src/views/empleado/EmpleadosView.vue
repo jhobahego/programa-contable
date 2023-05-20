9635bd07d13e51031019e85ee90d42957dc7d952
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-19 19:57:52 -0500

    Feat: vista correcta de empleados

--- src/views/empleado/EmpleadosView.vue
+++ src/views/empleado/EmpleadosView.vue
@@ -4,27 +4,31 @@ import { onMounted, ref } from 'vue';
 const empleados = ref(null)
 
 onMounted(async () => {
-    try {
-        const respuesta = await fetch("https://backend-etica.onrender.com/empleados");
-        if (!respuesta.ok) return;
+	try {
+		const respuesta = await fetch("https://backend-etica.onrender.com/empleados");
+		if (!respuesta.ok) return;
 
-        empleados.value = await respuesta.json();
-        console.log(empleados.value);
-    } catch (error) {
-        console.log(error);
-    }
+		empleados.value = await respuesta.json();
+		console.log(empleados.value);
+	} catch (error) {
+		console.log(error);
+	}
 })
 
 </script>
 
 <template>
-    <main>
-        <article class= "card__empleados" v-for="empleado in empleados">
-            <h3 class="card__title">nombre: {{ empleado.nombres }}</h3>
-            <p class="card__description">numero de cedula: {{ empleado.num_cedula }}</p>
-            <p class="card__description">salario: {{ empleado.salario }}</p>
-        </article>
-    </main>
+	<main>
+		<section class="card__container">
+			<article class="card__empleados" v-for="empleado in empleados" :key="empleado._id">
+				<div>
+					<h4 class="card__title">nombre: {{ empleado.nombres }}</h4>
+					<p class="card__description">numero de cedula: {{ empleado.num_cedula }}</p>
+					<p class="card__description">salario: {{ empleado.salario }}</p>
+				</div>
+			</article>
+		</section>
+	</main>
 </template>
 
 <style>
