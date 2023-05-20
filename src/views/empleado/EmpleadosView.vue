de571c2fa7dd21ebd9c07aa8cc45b514b02eb755
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-19 20:06:16 -0500

    Style: separación entre los datos

--- src/views/empleado/EmpleadosView.vue
+++ src/views/empleado/EmpleadosView.vue
@@ -21,11 +21,9 @@ onMounted(async () => {
 	<main>
 		<section class="card__container">
 			<article class="card__empleados" v-for="empleado in empleados" :key="empleado._id">
-				<div>
-					<h4 class="card__title">nombre: {{ empleado.nombres }}</h4>
-					<p class="card__description">numero de cedula: {{ empleado.num_cedula }}</p>
-					<p class="card__description">salario: {{ empleado.salario }}</p>
-				</div>
+				<h4 class="card__name">nombres: {{ empleado.nombres }}</h4>
+				<p class="card__description">numero de cedula: {{ empleado.num_cedula }}</p>
+				<p class="card__description">salario: {{ empleado.salario }}</p>
 			</article>
 		</section>
 	</main>
