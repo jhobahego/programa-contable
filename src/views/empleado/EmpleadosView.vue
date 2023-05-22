386e2bddb45330f997acc4d2fd8d9801ac9d9c22
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-21 19:06:13 -0500

    Style: mensaje al no haber empleados registrados

--- src/views/empleado/EmpleadosView.vue
+++ src/views/empleado/EmpleadosView.vue
@@ -16,7 +16,6 @@ onMounted(async () => {
 		if (!respuesta.ok) return;
 
 		empleados.value = await respuesta.json();
-		console.log(empleados.value);
 	} catch (error) {
 		console.log(error);
 	}
@@ -26,12 +25,12 @@ onMounted(async () => {
 
 <template>
 	<main>
-		<section class="card__container">
-			<div v-if="empleados.length === 0">
-				<p>No hay empleados registrados</p>
-				<RouterLink class="navbar__link" to="/">Registrar empleado</RouterLink>
-			</div>
-			<article v-else class="card__empleados" v-for="empleado in empleados" :key="empleado._id">
+		<div v-if="empleados?.length === 0">
+			<p>No hay empleados registrados</p>
+			<RouterLink class="empleados__link" to="/">Registrar empleado</RouterLink>
+		</div>
+		<section class="card__container" v-else>
+			<article class="card__empleados" v-for="empleado in empleados" :key="empleado._id">
 				<h4 class="card__name">nombres: {{ empleado.nombres }}</h4>
 				<p class="card__description">numero de cedula: {{ empleado.num_cedula }}</p>
 				<p class="card__description">salario: {{ empleado.salario }}</p>
