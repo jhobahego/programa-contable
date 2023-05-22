1daea70bbf28c8e0beb85d70047a14bffae63160
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-21 21:09:36 -0500

    Style: información y boton de redirección

--- src/views/empleado/EmpleadosView.vue
+++ src/views/empleado/EmpleadosView.vue
@@ -25,16 +25,18 @@ onMounted(async () => {
 
 <template>
 	<main>
-		<div v-if="empleados?.length === 0">
+		<div class="noresults" v-if="empleados?.length === 0">
 			<p>No hay empleados registrados</p>
 			<RouterLink class="empleados__link" to="/">Registrar empleado</RouterLink>
 		</div>
 		<section class="card__container" v-else>
 			<article class="card__empleados" v-for="empleado in empleados" :key="empleado._id">
-				<h4 class="card__name">nombres: {{ empleado.nombres }}</h4>
-				<p class="card__description">numero de cedula: {{ empleado.num_cedula }}</p>
-				<p class="card__description">salario: {{ empleado.salario }}</p>
-				<button @click="irADescontar(empleado.num_cedula)" >Descontar salario</button>
+				<div class="card__info">
+					<h4 class="card__name">nombres: {{ empleado.nombres }}</h4>
+					<p class="card__description">numero de cedula: {{ empleado.num_cedula }}</p>
+					<p class="card__description">salario: {{ empleado.salario }}</p>
+					<button class="card__btn" @click="irADescontar(empleado.num_cedula)" >Descontar salario</button>
+				</div>
 			</article>
 		</section>
 	</main>
